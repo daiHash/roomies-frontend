7a7f59e68e2331aab99bfe58abb18b0c2b7a6aa6
Daichi Hashimura <dhashi92@gmail.com>
2017-12-19 22:23:09 +0900

    Configure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// Home component
+import Home from '@/components/Home'
+
+// Reusable Global components
+import Header from '@/components/reusable/Header'
+import Footer from '@/components/reusable/Footer'
+
+// User components
+import Register from '@/components/user/Register'
+import Login from '@/components/user/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      components: {
+        default: Home,
+        header: Header,
+        footer: Footer
+      }
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      components: {
+        default: Register,
+        header: Header,
+        footer: Footer
+      }
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      components: {
+        default: Login,
+        header: Header,
+        footer: Footer
+      }
     }
   ]
 })
