f42e206cea0d2e4656f18d06aebc13c2ef76c307
Daichi Hashimura <dhashi92@gmail.com>
2017-12-28 19:23:19 +0900

    Create Pofile component

---
+++ src/components/user/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button type="button" @click="fetchProfiles">Profiles</button>
+    <h3>{{ profiles }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      profiles: ''
+    }
+  },
+  methods: {
+    fetchProfiles () {
+      this.axios.get('profiles')
+      .then(response => {
+        this.profiles = response.data
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
